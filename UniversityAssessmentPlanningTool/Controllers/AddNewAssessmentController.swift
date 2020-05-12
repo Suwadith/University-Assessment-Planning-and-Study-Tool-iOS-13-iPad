95c748b0adf89dd1ccf7ceb666619fbad43a1e11
Suwadith <suwadith.srithar@gmail.com>
2020-05-13 02:10:47 +0530

    designed the popover assessment input view

---
+++ UniversityAssessmentPlanningTool/Controllers/AddNewAssessmentController.swift
@@ -0,0 +1,9 @@
+//
+//  AddNewAssessmentController.swift
+//  UniversityAssessmentPlanningTool
+//
+//  Created by Suwadith on 5/13/20.
+//  Copyright © 2020 Suwadith. All rights reserved.
+//
+
+import Foundation
